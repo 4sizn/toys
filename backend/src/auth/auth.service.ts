723a8504a2ecd3ae5be3d1ad3dc5ecbfa1555c4e
4sizn <4sizn@naver.com>
2023-09-03 21:31:27 +0900

    feat: login access token

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   //로그인
   async signIn(userID: string, password: string): Promise<any> {
@@ -12,10 +16,9 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    const { password: _, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-
-    return result;
+    const payload = { sub: user.userID, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
